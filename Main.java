ec5e4b06a4500fc6fb383e30173a2e9a45dc3080
Pontbeatrix <106168177+Pontbeatrix@users.noreply.github.com>
2022-06-03 17:50:11 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,55 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        // 1. Printati "Hello" in consola, iar pe rand nou, printati numele vostru.
+        System.out.println("Pont Eniko"
+        );
+        // 2. Printati rezultatul sumei a doua numere(orice numere)
+        int x = 5;
+        double y = 6;
+        double z = (x + y);
+        System.out.println(z);
+        // 3. Printati rezultatul impartirii a doua numere. Atentie, daca numarul nu este intreg, sa afiseze si valorile dupa virgula.
+        double a = 28;
+        int b = 50;
+        double c = (a / b);
+        System.out.println(c);
+
+  // 4. Printati rezultatul urmatoarelor operatiuni:
+        // a. -5 + 8 * 6
+        int d = 8;
+        int e = 6;
+        short f = -5;
+        double g = (f+ e*d);
+        System.out.println(g);
+        // b. (55+9) % 9
+        double h = 55;
+        int j = 9;
+        short i = 9;
+        double m=( (h+j) %i );
+        System.out.println(m);
+        // c. 20 + -3*5 / 8
+        double n = 20;
+        float o= -3;
+        int p = 5;
+        int r = 8;
+        double s = (n+o*p/r);
+        System.out.println(s);
+        // d. 5 + 15 / 3 * 2 - 8 % 3
+         int  ab= 5;
+         double cd= 15;
+         int   ef= 3;
+         int   gh= 2;
+         int   nm= 8;
+         int   op= 3;
+        double rs = (ab+cd/ef*gh-nm%op);
+        System.out.println(rs);
+
+    }
+
+
+
+
+}
+
+
