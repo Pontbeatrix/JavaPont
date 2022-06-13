0737a66abfb525cd4178af60ef8c2e6d83d0d991
ENIKO PONT <pontebeatrix@gmail.com>
2022-06-13 21:47:28 +0300

    - Tema 1,2

---
+++ src/temperature.java
@@ -0,0 +1,16 @@
+public class temperature {
+    public void main(String arg[]) {
+        float fahrenheit, celsius;
+        fahrenheit = 89;
+        celsius = ((fahrenheit * 5) / 9) - 32;
+        System.out.println("Temperature in celsius is: " + celsius);
+
+//C = 5/9 * (F -32)
+    }
+
+
+}
+
+
+
+
